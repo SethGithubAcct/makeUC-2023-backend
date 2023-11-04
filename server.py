564010f8c0f873ff9885df252270369ab8774260
SethGithubAcct <seth.adkins@protonmail.com>
2023-11-04 18:46:33 -0400

    grab api key from secret manager

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from google.cloud import secretmanager_v1 as secmgr
 import openai
 import requests
 import json
 
+# setup
 app = Flask(__name__)
+client = secmgr.SecretManagerServiceClient()
+
+# get OpenAI key from secret manager
+api_secret = client.access_secret_version(
+    request=secmgr.AccessSecretVersionRequest(
+        name='projects/makeuc-2023/secrets/openai-api-key/versions/latest'
+    )
+)
+api_key = api_secret.payload.data
 
 @app.route("/analyze")
 def analyze():
-    return json.dumps({'hello': 'world'})
+    return json.dumps({'api-key': api_key.decode('utf-8')})
 
 app.run()
