118895d5699fdd32057ae4107ca2954f96f21dee
SethGithubAcct <seth.adkins@protonmail.com>
2023-11-04 20:33:13 -0400

    create requirements.txt

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import json
 # setup
 app = Flask(__name__)
 client = secmgr.SecretManagerServiceClient()
+model = "gpt-3.5-turbo" # change me to change GPT model
 
 # get OpenAI key from secret manager
 api_secret = client.access_secret_version(
@@ -14,10 +15,18 @@ api_secret = client.access_secret_version(
         name='projects/makeuc-2023/secrets/openai-api-key/versions/latest'
     )
 )
-api_key = api_secret.payload.data
+api_key = api_secret.payload.data.decode('utf-8')
+openai.api_key = api_key
 
 @app.route("/analyze")
 def analyze():
-    return json.dumps({'api-key': api_key.decode('utf-8')})
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {'role': "system", 'content': "You are an AI that analyzes programs for security vulnerabilites."},
+            {'role': "user", 'content': "What is a buffer overflow vulnerability?"}
+        ]
+    )
+    return json.dumps(response.choices[0].message)
 
 app.run()
